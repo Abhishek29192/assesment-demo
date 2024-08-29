1342a5d928a1214b49385a6dc734d0887524f502
apple <apple@apples-MacBook-Air.local>
2024-08-29 13:18:58 +0530

    initail commit

---
+++ src/component/Card/Card.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import styles from "./card.module.css";
+
+type CardProps = {
+  icon: ReactNode;
+  title: string;
+  value: string;
+};
+const Card = ({ icon, title, value }: CardProps) => {
+  return (
+    <div className={styles.cardWrapper}>
+      <div className={styles.cardIcon}>{icon}</div>
+      <div className={styles.cardText}>
+        <h4 className={styles.cardTitle}>{title}</h4>
+        <h2 className={styles.cardNumber}>{value}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
